21aa546c4d8261aa8d4e4a458addfa14e0b7b82b
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-04-29 12:47:54 +0530

    Update main.tf

--- terraform3/main.tf
+++ terraform3/main.tf
@@ -77,6 +77,13 @@ resource "aws_security_group" "strapi_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+  from_port   = 1337
+  to_port     = 1337
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
